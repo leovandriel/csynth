b9815dbd15e45a6f864f5e741218d8b847278734
Leo Vandriel <git@leovandriel.com>
2023-11-07 12:36:15 -0800

    add test selector

--- test.sh
+++ test.sh
@@ -2,7 +2,15 @@
 
 set -e
 
-find ./src -name "*.h" -not -path "./src/example/*" -print0 | while read -d $'\0' file; do
+if [ $# -eq 0 ]; then
+    pattern=".*\\.h"
+fi
+
+if [ $# -eq 1 ]; then
+    pattern=".*$1.*\\.h"
+fi
+
+find ./src -regex "$pattern" -not -path "./src/example/*" -print0 | while read -d $'\0' file; do
     name=$(basename "$file" .h)
     printf "#include \".$file\"\\nint main() { test_$name(); return 0; }" > ./src/test.c
     gcc ./src/test.c -o bin/test -Wall -Wextra -lportaudio
