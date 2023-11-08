7f9e5393ce83f559a3c966141eb19a4b254b0523
Leo Vandriel <git@leovandriel.com>
2023-11-08 11:22:34 -0800

    tweak base drummer

--- src/example/drummer.h
+++ src/example/drummer.h
@@ -3,8 +3,8 @@
 
 func drummer()
 {
-    func snare = loop_(mul(noise(), decay_(.05)), 1);
-    func bdrum = loop_(delay_(mul(sine(linear_(60, 30, 1)), decay_(.05)), .5), 1);
+    func snare = loop_(delay_(mul(noise(), decay_(.05)), .5), 1);
+    func bdrum = loop_(mul(sine(linear_(60, 30, 1)), decay_(.05), _(3)), 1);
     func hihat = loop_(mul(high_pass_(noise(), 20000), decay_(.05)), .25);
     return mul_(add(snare, bdrum, hihat), .4);
 }
