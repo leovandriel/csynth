8e492ad401eb5a1da40348ddb8298c32e73b01ef
Leo Vandriel <git@leovandriel.com>
2024-05-05 19:55:17 +0200

    cleanup continuous

--- examples/dynamic.c
+++ examples/dynamic.c
@@ -5,34 +5,38 @@
 #define P(_f) (mul(_f, pitch))
 #define T(_f) (dvd(_(_f), speed))
 
-static func beep_boop_dynamic(func pitch, func speed)
+static func beep_dynamic(func pitch, func speed)
 {
-    func high = sine(P(A4));
-    func low = mul_(saw(P(A2)), .5);
-    func beep = mul(high, block(T(0), T(.1)));
-    func boop1 = mul(low, block(T(.4), T(.5)));
-    func boop2 = mul(low, block(T(1), T(1.1)));
-    func melody = add(beep, boop1, boop2);
-    func looped = loop(melody, T(1.6));
-    func echoed = reverb(looped, T(.2), _(.5));
-    func scaled = mul_(echoed, .1);
-    return scaled;
+    func tone = sine(P(A4));
+    func note = mul(tone, block(T(0), T(.3)), _(.5));
+    func looped = loop(note, T(1.5));
+    func revved = reverb(looped, T(.4), _(.2));
+    return revved;
 }
 
 func wobble_both()
 {
     func wobble = continuous(add_(mul_(sine_(.1), .1), 1));
-    return beep_boop_dynamic(wobble, wobble);
+    return beep_dynamic(wobble, wobble);
 }
 
 func modulate_pitch()
 {
     func modulate = continuous(add_(mul_(timer(), .5), .01));
-    return beep_boop_dynamic(modulate, ONE);
+    return beep_dynamic(modulate, ONE);
 }
 
 func modulate_speed()
 {
     func modulate = continuous(add_(mul_(timer(), .5), .01));
-    return beep_boop_dynamic(ONE, modulate);
+    return beep_dynamic(ONE, modulate);
+}
+
+int main()
+{
+    func dynamic = comp_rel(
+        modulate_pitch(), _(10),
+        modulate_speed(), _(10),
+        wobble_both(), _(10));
+    return play(dynamic, 30);
 }
--- src/func/effect/continuous.h
+++ src/func/effect/continuous.h
@@ -1,6 +1,12 @@
 //
 // continuous.h - Prevents its input from being reset
 //
+// Funcs can call `gen_reset` on their input to revert the state, e.g. in `loop`
+// to allow for looping. The `continuous` func prevents the propagation of such
+// a reset, allowing the func argument to continue independently of such a loop.
+// This allows for combining looped and non-looped funcs in cases where nesting
+// is required. See `dynamic.c` for an example.
+//
 // `continuous(input)`
 //
 #ifndef CSYNTH_CONTINUOUS_H
@@ -8,6 +14,11 @@
 
 #include "../../util/test.h"
 #include "../../core/func.h"
+#include "./loop.h"
+#include "../env/envs.h"
+
+double continuous_eval(__attribute__((unused)) int count, Gen **args, __attribute__((unused)) double delta, __attribute__((unused)) void *context);
+// implemented in func.h
 
 Func *continuous(Func *input)
 {
@@ -16,6 +27,13 @@ Func *continuous(Func *input)
 
 void test_continuous()
 {
+    Func *t = loop_(continuous(linear_(0, 1, 1)), 2);
+    Gen *g = gen_create(t, 1);
+    assert_gen_equal(g, 0);
+    assert_gen_equal(g, 1);
+    assert_gen_equal(g, 2);
+    assert_gen_equal(g, 3);
+    assert_gen_equal(g, 4);
 }
 
 #endif // CSYNTH_CONTINUOUS_H
