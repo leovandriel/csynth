9243dfec37ff348f0c142ad49eeb0fd8f6ff19ce
Leo Vandriel <git@leovandriel.com>
2024-06-20 22:02:51 -0700

    add test

--- tests/util/rand.h
+++ tests/util/rand.h
@@ -7,7 +7,13 @@ void test_rand()
     assert(rand_unsigned_long() == 1152992998833853505UL);
     assert(rand_unsigned_long() == 11177516664432764457UL);
     assert(rand_unsigned_long() == 17678023832001937445UL);
-    assert(rand_range(0, 1) == 0.52362249432221674716);
-    assert(rand_range(1, 2) == 1.96362799473474725076);
-    assert(rand_range(-3, 7) == 3.33880676916069862869);
+    assert(rand_uniform() == 0.52362249432221674716);
+    assert(rand_uniform() == 0.96362799473474713973);
+    assert(rand_uniform() == 0.63388067691606986286);
+    assert(rand_range(0, 1) == 0.77411840881254556645);
+    assert(rand_range(1, 2) == 1.48922748458651099667);
+    assert(rand_range(-3, 7) == 1.51714985351188635931);
+    assert(rand_gauss(0, 1) == 1.92496688266798221533);
+    assert(rand_gauss(1, 2) == -1.81970181198147629153);
+    assert(rand_gauss(-2, 3) == -0.74949711474660718479);
 }
