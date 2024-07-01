cf8aa5e64dda449e09d66268d41b5de241563f60
Leo Vandriel <git@leovandriel.com>
2024-07-01 12:56:19 -0700

    synth tweaks

--- examples/demo/midi_synth.c
+++ examples/demo/midi_synth.c
@@ -8,13 +8,13 @@ int main()
     func hpf_knob = gauge("hpf", knob_ex_(1, 71, 1, 40000));
     func distortion_knob = gauge("distort", knob_(1, 72, 0, 10));
     func overtone_knob = gauge("overtone", knob_(1, 73, -10, 1));
-    func detune_knob = gauge("detune", knob_ex_(1, 74, 0.001, 1.0));
+    func detune_knob = gauge("detune", knob_ex_(1, 74, .001, 1.0));
     func reverb_time = gauge("rev-time", knob_(1, 75, 0, .5));
-    func reverb_decay = gauge("rev-decay", knob_(1, 76, 0, .99));
+    func reverb_decay = gauge("rev-decay", knob_ex_(1, 76, .01, .99));
     func post_gain = gauge("gain", knob_(1, 77, 1, 10));
     func note = unison(5, detune_knob, saw(C0));
     func synth = midi_keyboard(1, key, note);
-    synth = blend_(1, 2, synth);
+    synth = blend_(1, 1.06, synth);
     synth = overtone(-4, overtone_knob, synth);
     synth = distortion(distortion_knob, synth);
     synth = hpf(hpf_knob, synth);
