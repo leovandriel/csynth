7cd61acc92c859aebfdb322c1743665d84c8b342
Leo Vandriel <git@leovandriel.com>
2024-10-14 09:28:24 -0700

    add pattern demo

--- examples/func/comp/pattern.c
+++ examples/func/comp/pattern.c
@@ -2,11 +2,25 @@
 #include "../../../src/func/all.h"
 #include "../../../src/io/player.h"
 
+func note(func frequency)
+{
+    return mul_(.5, decay_(.2, unison_(4, .01, saw(frequency))));
+}
+
+func arp(func note1, func note2, func note3)
+{
+    return add(
+        pattern_(".  .  . ", .25, .01, note(note1)),
+        pattern_(" .  .   ", .25, .01, note(note2)),
+        pattern_("  .  . .", .25, .01, note(note3)));
+}
+
 int main(void)
 {
-    func patterns = add(
-        pattern_("................", .25, .01, hihat()),
-        pattern_("  .   .   .   . ", .25, .01, snare()),
-        pattern_(".    .  .    .  ", .25, .01, bdrum()));
-    return play(loop_(4, patterns));
+    func comp = add(
+        pattern_(".   .   ", 2, .01, arp(F4, A4, C5)),
+        pattern_(" .   .  ", 2, .01, arp(G4, B4, D5)),
+        pattern_("  .   . ", 2, .01, arp(E4, G4, B4)),
+        pattern_("   .   .", 2, .01, arp(F4, A4, C6)));
+    return play(tempo_(1.5, comp));
 }
