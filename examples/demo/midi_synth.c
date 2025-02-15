7536e9f4ea467971cf942f67a4fe9bf87a41d8dc
Leo Vandriel <git@leovandriel.com>
2025-02-14 22:29:38 -0800

    tweaks

--- examples/demo/midi_synth.c
+++ examples/demo/midi_synth.c
@@ -9,7 +9,7 @@ int main(void)
     func_print(lpf_knob);
     func hpf_knob = gauge("hpf", knob_ex_(1, 71, 1, 20000));
     func distortion_knob = gauge("distort", knob_(1, 72, 0, 2));
-    func overtone_knob = gauge("overtone", knob_(1, 73, -10, 1));
+    func overtone_knob = gauge("overtone", knob_(1, 73, -10, 0));
     func detune_knob = gauge("detune", knob_ex_(1, 74, .001, 1.0));
     func reverb_time = gauge("rev-time", knob_(1, 75, 0, .5));
     func reverb_decay = gauge("rev-decay", knob_ex_(1, 76, .01, .99));
@@ -25,5 +25,5 @@ int main(void)
     synth = reverb(reverb_time, reverb_decay, synth);
     synth = mul(post_gain, synth);
     func drums = add(pad(10, 40, snare()), pad(10, 41, bdrum()), pad(10, 42, hihat()));
-    return play_midi(fps("fps", add(mul_(.2, synth), drums)));
+    return play_midi(fps("fps", add(mul_(.2, synth), mul_(5, drums))));
 }
